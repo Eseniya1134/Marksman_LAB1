bb7b962c4ffdbe719b629dec05e4e3c5baa1e0d4
Eseniya1134 <shakirova.esya23@mail.ru>
2026-03-03 02:30:26 +0300

    fxml created

--- src/main/java/com/marksman/Main.java
+++ src/main/java/com/marksman/Main.java
@@ -1,16 +1,20 @@
 package com.marksman;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 800, 600);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/marksman/game.fxml"));
+        BorderPane root = loader.load();
+        Scene scene = new Scene(root, 1000, 600);
         stage.setTitle("Меткий стрелок");
         stage.setScene(scene);
         stage.show();
